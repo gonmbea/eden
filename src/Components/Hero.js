d46bd6bc03c7f6815f0f9a6293082ded4af13e7e
gonmbea <beatrizgonmoura@gmail.com>
2025-04-14 15:58:20 -0300

    fix: centering issues with navbar and hero

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import '../Scss/_hero.scss';
 import edenLogo from '../Assets/edenLogo.png';
+// import { useTranslation } from "react-i18next";
+
 
 
 function Hero() {
+  // const { t } = useTranslation();
+  
   return(
   <div className='hero-container'>
 
     <div className='hero-container-logo'>
         <img src={edenLogo}/>
-        {/* <img src={adobeLogo}/> */}
     </div>
     <div className='hero-container-text'>
         <h1>WELCOME</h1>
