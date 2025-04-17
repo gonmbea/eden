2f697a848e33e3a0b8d624293de7b77ee6e27a74
gonmbea <beatrizgonmoura@gmail.com>
2025-04-17 15:10:58 -0300

    feat: button that toggles language added and logo to navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,25 +1,36 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation} from "react-router-dom";
 import '../Scss/_navBar.scss';
 import { useTranslation } from "react-i18next";
 import { HashLink as AnchorLink } from 'react-router-hash-link';
-
+import edenLogo from '../Assets/edenLogo.png';
+import i18n from '../i18n';
 
 
 function NavBar() {
     const { t } = useTranslation("NavBar");
+
+    const location = useLocation();
+    const isHomePage = location.pathname === "/";
     
     
-    // const toggleLanguage = () => {
-    //     const newLang = i18n.language === "en" ? "pr" : "en";
-    //     i18n.changeLanguage(newLang);
-    // };
+    const toggleLanguage = () => {
+        const newLang = i18n.language === "en" ? "pt" : "en";
+        i18n.changeLanguage(newLang);
+    };
 
     return (
         <div className="navbar">
-            {/* <div className="navbar-logo">
-                <img src="../Assets/edenLogo.png"/>
-            </div> */}
+
+            {/* Logo which will be hidden in the home page */}
+            {/* {!isHomePage && ( */}
+                <div className="navbar-logo">
+                    <Link to="/">
+                        <img src={edenLogo} alt="Eden Logo"/>
+                    </Link>
+                </div>
+            {/* )} */}
+
             <div>
             <ul className="navbar-links">
                     <li>
@@ -49,6 +60,13 @@ function NavBar() {
                     </li>
                 </ul>
 
+            </div>
+
+            {/* Button that switches langueages */}
+            <div className="navbar-lang">
+                <button onClick={toggleLanguage} className="navbar-lang-button">
+                    {i18n.language === "en" ? "PT" : "EN"}
+                </button>
             </div>
         </div>
     );
--- src/i18n.js
+++ src/i18n.js
@@ -2,16 +2,16 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
 import enJSON from './locale/en.json';
-import prJSON from './locale/pr.json';
+import ptJSON from './locale/pt.json';
 
 i18n.use(initReactI18next).init({
     resources: {
         en: { ...enJSON },
-        pr: { ...prJSON }
+        pt: { ...ptJSON }
     },
     lng: "en",
     fallbackLng: "en",
 
 })
 
-// export default i18n;
+export default i18n;
