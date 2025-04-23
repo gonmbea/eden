05b2169b319c1a5761065cccad2cdb5d3f3172a6
gonmbea <beatrizgonmoura@gmail.com>
2025-04-23 18:28:40 -0300

    styling: underline hover animation added

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../Scss/_footer.scss';
 import { useTranslation } from "react-i18next";
 
+import { motion } from "framer-motion";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
@@ -14,21 +15,28 @@ function Footer() {
     return (
         <footer className="footer-container">
             <div className="footer-icons">
-                <div>
-                <a href='https://www.instagram.com/meloboutique.b?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==' target="_blank">
-                        <FontAwesomeIcon icon={faInstagram} className='icon'/>
-                    </a>
-                </div>
-                <div>
-                    <a href='https://www.linkedin.com/in/gonmbea/' target="_blank">
-                        <FontAwesomeIcon icon={faLinkedinIn} className='icon'/>
-                    </a>
-                </div>
-                <div>
-                    <a href="mailto:beatrizgonmoura@gmail.com?body=Write me something here :)">
-                        <FontAwesomeIcon icon={faEnvelope}  className='icon'/>
-                    </a>
-                </div>
+                <motion.a
+                    href="https://www.instagram.com/meloboutique.b?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
+                    target="_blank"
+                    whileHover={{ scale: 1.2 }}
+                >
+                    <FontAwesomeIcon icon={faInstagram} className="icon" />
+                </motion.a>
+
+                <motion.a
+                    href="https://www.linkedin.com/in/gonmbea/"
+                    target="_blank"
+                    whileHover={{ scale: 1.2 }}
+                >
+                    <FontAwesomeIcon icon={faLinkedinIn} className="icon" />
+                </motion.a>
+
+                <motion.a
+                    href="mailto:beatrizgonmoura@gmail.com?body=Write me something here :)"
+                    whileHover={{ scale: 1.2 }}
+                >
+                    <FontAwesomeIcon icon={faEnvelope} className="icon" />
+                </motion.a>
             </div>
 
             <div className="footer-copyright">
